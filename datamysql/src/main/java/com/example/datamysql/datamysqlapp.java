469ece3eee556af81827482409e0d2a1604e97a2
Olumide Omololu <oaomololu@gmail.com>
2020-10-31 22:39:11 +0100

    Add files via upload

---
+++ datamysql/src/main/java/com/example/datamysql/datamysqlapp.java
@@ -0,0 +1,11 @@
+package com.example.datamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class datamysqlapp {
+    public static void main(String[] args) {
+        SpringApplication.run(datamysqlapp.class, args);
+    }
+}
